08ecbc5fab2fb51a36d950bdcfe6305092a9e109
fm750 <1392245102@qq.com>
2025-02-25 09:32:10 +0800

    Add files via upload

---
+++ py002.py
@@ -0,0 +1,34 @@
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+#
+# print("Hello Wrold")
+# print("Hello Wrold")
+# print("Hello Wrold")
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# 123
+# # 123
+# # 123
+# print("hello",end="/")
+# # print("world")
+# age=18
+# name="bingbing"
+# # print("你的名字：%s,你的年龄:%d"%(name,age))
+# a = 123
+# print=("%06d" % a)
+a=123
+print("%06d" % a)
